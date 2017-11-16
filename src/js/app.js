a0bb2e77a26aebab3f63a71b445e23362da599de
Michael <michaelkovarschool@gmail.com>
2017-11-16 13:00:07 -0600

    Added functionality to the program, final commit before transitioning to using Express as a proxy and running React seperatly with webpack

--- src/js/app.js
+++ src/js/app.js
@@ -1,34 +1,45 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+//import { renderToString } from 'react-dom/server';
+import {default as admin} from 'firebase-admin';
+//import React from 'react'
 //this is server side stuff
-
 /*
 EXPRESS SET UP
 */
 //sets up express initally
-const express = require('express');
+import express from 'express';
+//const express = require('express');
 const app = express();
 
 //sets up express for the react views
-app.set('view engine','jsx');
-app.engine('jsx',require('express-react-views').createEngine());
-app.set('views','./src/views');
+//app.set('view engine','jsx');
+///app.engine('jsx',require('express-react-views').createEngine());
+//app.set('views','./src/views');
 
 /*
 FIREBASE SETUP
 */
-//sets up the admin creditionals
-var admin = require("firebase-admin");
-var serviceAccount = require("../../creditionals/firebase-secret.json");
+//sets up the admin creditionals - this could be done as a import, but i think that it makes more sense reading the code to do it
+//this way.
+var serviceAccount = require("../../credentials/firebase-secret.json");
 //inializes the app
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://testing-cb46e.firebaseio.com"
+  databaseURL: "https://testing-cb46e.firebaseio.com",
+  databaseAuthVariableOverride: {
+    uid: "server"
+  },
 });
-
+const database = admin.database();
 /*
 CODE
 */
+/*ReactDOM.render(<Article/>,document.getElementById("appHolder")
+);*/
 app.get('/', (req, res) => {
-	res.render('article',{title:'<3',paragraph:"Now, this is a story all about how. My life got flipped-turned upside down. And I'd like to take a minute. To sit right there I'll tell you how I became the prince of a town called Bel Air."});
+	res.send("Hello World!");
 });
 //initalizes the listening on port 3000 for website related traffic.
+//this is now on 3000
 app.listen(3000,()=>{console.log("listening on port 3000")});
---
+++ src/js/test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { renderToString } from 'react-dom/server';
+//var React = require("react");	
+
+export defautl class Article extends React.Component{
+	render(){
+		return[
+			{props}
+		];
+	}
+	renderAsString(){
+		return renderToString(render());
+	}
+}
